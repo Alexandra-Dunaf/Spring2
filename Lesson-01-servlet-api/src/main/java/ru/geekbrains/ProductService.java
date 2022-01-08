da7ed85720b259efc746d64c30fae704b05dd888
Sanya <sashaklgn@gmail.com>
2022-01-08 19:12:04 +0400

    Initial commit

---
+++ Lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductService.java
@@ -0,0 +1,15 @@
+package ru.geekbrains;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProductList();
+
+    void saveOrUpdate(Product product);
+
+    Product getProductById(Long id);
+
+    void deleteById(Long id);
+
+}
